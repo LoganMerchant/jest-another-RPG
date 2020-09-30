718de7fc2620075f49d92b6b44814fd49cb68460
Logan Merchant <merchantclogan@gmail.com>
2020-09-29 22:48:48 -0600

    add mock data for Player's potion inventory

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,4 +1,11 @@
 const Player = require('../lib/Player.js');
+const Potion = require('../lib/Potion.js');
+
+// Makes sure to use the mock data in `./lib/__mocks__/Potion.js` for testing purposes
+// rather than the data in `Potion.js`
+jest.mock('../lib/Potion.js');
+
+console.log(new Potion());
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -7,4 +14,8 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
 });
